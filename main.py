25bf2ad2db37aaf1772c2c01b37c21465b7dccde
Polina Pototska <po.pototska@gmail.com>
2023-01-21 10:07:01 +0100

    add background image

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pygame
+import os 
 pygame.init()
+
+def file_path(file_name):
+    folder_path = os.path.abspath(__file__ + "/..")
+    path = os.path.join(folder_path, file_name)
+    return path
 
 FPS = 40
 WIN_WIDTH = 700
@@ -8,6 +14,9 @@ WIN_WEIGHT = 500
 window = pygame.display.set_mode((WIN_WIDTH, WIN_WEIGHT))
 clock = pygame.time.Clock()
 
+background = pygame.image.load(file_path("galaxy.jpg"))
+background = pygame.transform.scale(background, (WIN_WIDTH, WIN_WEIGHT))
+
 play = True
 game = True
 while game == True:
@@ -15,5 +24,7 @@ while game == True:
         if event.type == pygame.QUIT:
             game = False
 
+    if play == True:
+        window.blit(background, (0, 0))
     clock.tick(FPS)
     pygame.display.update()
